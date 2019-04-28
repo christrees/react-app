cd827597bbe39b91a24d748cef43647de1a685f0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-28 02:59:59 +0000

    Deploy christrees/react-app to github.com/christrees/react-app.git:gh-pages

---
+++ precache-manifest.918148e732a932422b3df28d94182d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef4e99cd0c6c881b85a5",
+    "url": "/react-app/static/css/main.25207f4f.chunk.css"
+  },
+  {
+    "revision": "ef4e99cd0c6c881b85a5",
+    "url": "/react-app/static/js/main.1d4f3f13.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "a8d4a8104357ac9bf7f7",
+    "url": "/react-app/static/js/2.da3641c5.chunk.js"
+  },
+  {
+    "revision": "6749bb2597ec5de5aee21a686f25280d",
+    "url": "/react-app/index.html"
+  }
+];
